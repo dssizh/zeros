3b7c06e82c82332dfd7e83fbcae26327880979d7
Dmitriy S <sutyginds@gmail.com>
2019-09-25 10:33:12 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+  let factorialArray = [];
+  factorialArray = expression.split(/\*/);
+
+  countOfFive = 0;
+  countOfTwo = 0;
+  for (i = 0; i < factorialArray.length; i++) {
+    strFactorial = factorialArray[i];
+    start = 1;
+    step = 1;
+    numFactorial = 0;
+    if (strFactorial.indexOf("!!") != -1) {
+      step = 2;
+      numFactorial = strFactorial.substr(0, strFactorial.length - 2);
+      if (numFactorial % 2 == 0) {
+        start = 2;
+      }
+    } else if (strFactorial.indexOf("!") != -1) {
+      numFactorial = strFactorial.substr(0, strFactorial.length - 1);
+    }
+
+    for (j = start; j <= numFactorial; j = j + step) {
+      currentNumber = j;
+
+      while ((currentNumber % 5 == 0) && (currentNumber > 1)) {
+        countOfFive++;
+        currentNumber = currentNumber / 5;
+      }
+
+      while ((currentNumber % 2 == 0) && (currentNumber > 1)) {
+        countOfTwo++;
+        currentNumber = currentNumber / 2;
+      }
+    }
+  }
+  return Math.min(countOfFive, countOfTwo);
 }
